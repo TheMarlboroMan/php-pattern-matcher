7a0b76914a46bb17cca91a120768c04207cc1ad4
TheMarlboroMan <marlborometal@gmail.com>
2018-06-12 21:54:41 +0200

    Somewhat working, needs better naming

--- lib/src/tools/pattern_matcher.php
+++ lib/src/tools/pattern_matcher.php
@@ -1,32 +1,66 @@
 <?php
 namespace tools;
 
+//TODO: Document this.
 
-/*
-//TODO: This could be an example format...
-path/{id: number}/and/{name:string}/finally/{something:any}/and_done
+//TODO: CHOOSE BETTER NAMES!
 
-number: 0-9
-string: a-zA-z-_
-any: .
+//TODO: Perhaps we have different classes for different types.
+class pattern_match {
 
-translated to
+	private $name;
+	private $value;
 
-path/[0-9]+/and/[a-zA-Z\-_]+/finally/[.+]/and_done
-*/
+	public function __construct($_n, $_v) {
+		$this->name=$_n;
+		$this->value=$_v;
+	}
 
-//TODO
+	public function	get_name() {
+		return $this->name;
+	}
+
+	public function get_value() {
+		return $this->value;
+	}
+}
+
+class pattern_result {
+
+	private	$success;
+	private $name;
+	private $params=[];
+
+	public function __construct($_s, $_n, array $_m) {
+		$this->success=$_s;
+		$this->name=$_n;
+		$this->params=$_m;
+	}
+
+	public function is_match() {
+		return $this->success;
+	}
+
+	public function get_name() {
+		return $this->name;
+	}
+
+	public function get_parameters() {
+		return $this->params;
+	}
+}
 
 interface pattern_chunk {
-	//_s is the full string, _i is the index where the previous match left,
-	//and must be written by each pattern_chunk.
-	public function match($_s, &$_i);
+	//!_s is the full string, _i is the index where the previous match left,
+	//!and must be written by each pattern_chunk. $_res is where the params are
+	//!written.
+	public function match($_s, &$_i, array &$_res);
 }
 
 class pattern_chunk_fixed implements pattern_chunk{
 
-	public $str;
-	public $len;
+	private $str;
+	private $len;
 
 	public function __construct($_s) {
 
@@ -34,10 +68,7 @@ class pattern_chunk_fixed implements pattern_chunk{
 		$this->len=strlen($this->str);
 	}
 
-	public function match($_v, &$_i) {
-
-//print_r($this);
-//echo $_v.' '.$_i.PHP_EOL;
+	public function match($_v, &$_i, array &$_res) {
 
 		$part=substr($_v, $_i, $this->len);
 		$_i+=$this->len;
@@ -53,10 +84,13 @@ class pattern_chunk_match implements pattern_chunk {
 	const literal_integer='int';
 	const literal_string='string';
 
-	public $type;
+	private $type;
 	public $name;
+	private $last_character;
 
-	public function __construct($_s) {
+	public function __construct($_s, $_l) {
+
+		$this->last_character=$_l;
 
 		if(false===strpos($_s, self::chr_separate)) {
 			//TODO: THrow real exception.
@@ -91,14 +125,33 @@ class pattern_chunk_match implements pattern_chunk {
 		}
 	}
 
-	public function match($_v, &$_i) {
+	public function match($_v, &$_i, array &$_res) {
 
-		//TODO
-		//UP TO HOW MUCH DO WE READ???
+		$last_index=strlen($_v)-1;
+		$val='';
 
-print_r($this);
-echo $_v.' '.$_i.PHP_EOL;
-die();
+		for($_i; ; $_i++) {
+
+			//We might have arrived at the end of the test string. If we didn't break we are fine.
+			if($_i==$last_index && null===$this->last_character) {
+				break;
+			}
+
+			$char=$_v[$_i];
+			//Or perhaps we are a the end of the current match...
+			if($char==$this->last_character) {
+				break;
+			}
+
+			switch($this->type) {
+				//TODO: What about negative numbers?
+				case self::type_integer: if(!ctype_digit($char)) return false; break;
+				case self::type_string: if(!ctype_alpha($char)) return false; break;
+			}
+		}
+
+		$_res[]=new pattern_match($this->name, $val);
+		return true;
 	}
 
 }
@@ -120,26 +173,35 @@ class pattern {
 		$this->raw_pattern=$_p;
 		$this->id=$_i;
 
-		$this->prepare_pattern();
-		$this->check_pattern();
+		$this->prepare();
+		$this->check_integrity();
 	}
 
 	public function		matches($_input) {
 
-		echo 'matching '.$this->raw_pattern.' to '.$_input.PHP_EOL;
+//echo 'matching '.$this->raw_pattern.' to '.$_input.PHP_EOL;
 
+		$results=[];
 		$index=0;
+		$max_index=strlen($this->raw_pattern);
 		foreach($this->chunks as $v) {
 
-			if(!$v->match($_input, $index)) {
-				return false;
+			if(false===$v->match($_input, $index, $results)) {
+				return null;
+			}
+
+			//The input might have no variables and might be shorter than the pattern...
+			//If we have gotten to far, we have approved all matches, so everything
+			//is good.
+			if($index >= $max_index) {
+				break;
 			}
 		}
 
-		return true;
+		return new pattern_result(true, $this->id, $results);
 	}
 
-	private function	prepare_pattern() {
+	private function	prepare() {
 
 		$current='';
 		$mode=self::mode_literal;
@@ -170,7 +232,8 @@ class pattern {
 						case self::mode_literal:
 							$this->chunks[]=new pattern_chunk_fixed($current); break;
 						case self::mode_pattern:
-							$this->chunks[]=new pattern_chunk_match($current); break;
+							$next_character=$index==$last_index ? null : $this->raw_pattern[$index+1];
+							$this->chunks[]=new pattern_chunk_match($current, $next_character); break;
 					}
 
 					$current='';
@@ -180,7 +243,7 @@ class pattern {
 		}
 	}
 
-	private function check_pattern() {
+	private function check_integrity() {
 
 		//TODO: Throw real shit.
 		if(!count($this->chunks)) {
@@ -190,6 +253,8 @@ class pattern {
 		if($this->chunks[0] instanceof pattern_chunk_match) {
 			throw new \Exception("the first part of a pattern cannot be a match type for '$this->raw_pattern'");
 		}
+
+		//TODO: Check for duplicate names!!!!
 	}
 }
 
@@ -206,21 +271,24 @@ class pattern_matcher {
 	public function 	match($_input) {
 
 		foreach($this->list as $k => $v) {
-			if($v->matches($_input)) {
-				//TODO: Return a dedicated result object that will the parameter values...
-				return $v;
+
+			$r=$v->matches($_input);
+			if(null!==$r) {
+				return $r;
 			}
 		}
 
-		return null;
+		return new pattern_result(false, null, []);
 	}
 
 	//TODO: Build from file.
 	private function 	setup_list() {
 
 		$this->list=[
-			new pattern("let/[type:string]/thing/[id:int]/and/[val:string]/index.html", "composite_path"),
-			new pattern("let/another_string", "another_string"),
+			new pattern("let/[type:int]/thing/[id:int]/and/[val:string]/index.html", "composite_path"),
+			new pattern("let/another_string/[val:string]", "another_string"),
+			new pattern("let/[type:int]/end", "final"),
+			new pattern("simple-path", "simple"),
 		];
 	}
 }
